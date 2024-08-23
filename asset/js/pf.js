388bb4fc518d08ef7f67bbaf903bb82fbf51d576
Patrice976 <catiatp@gmail.com>
2024-08-23 16:35:57 +0200

    mise en page display bouton

--- asset/js/pf.js
+++ asset/js/pf.js
@@ -1,3 +1,4 @@
+// variable première section 
 
 let imgMe = document.getElementById('me')
 let body = document.querySelector('body')
@@ -5,8 +6,19 @@ let header = document.querySelector('header')
 let blue = false
 let passion = document.querySelector('.passions')
 
+//variable troisième section 
+//Objet stack/text 
+const StackToText = {
+    'img_js' : 'txt_js',
+    'img_python' : 'txt_python'
+}
+
+imgSec3= document.querySelector('.stack_img img')
+
+console.log(document)
+// première section : changement de theme au survol de l'image 
+
 imgMe.addEventListener('mouseover', ()=>{
-console.log('hi')
     if(blue === false) { 
             imgMe.style.opacity = 0 ;
             setTimeout(()=> {
@@ -33,23 +45,69 @@ console.log('hi')
         document.documentElement.style.setProperty('--secondary-dark-color', '#40261D');
         blue = false
     }})
+//troisième section : Quand on appuie sur un des boutons, le contenu de ce dernier s'afficher 
+
+//Créer un objet qui lie mes boutons et mes éléments d'affichage 
+
+let buttonMaStack = document.getElementById('stack')
+let displayStack = document.querySelector('.ma_stack')
+let buttonOutils = document.getElementById('outils')
+let displayOutils = document.querySelector('.mes_outils')
+let buttonSkills = document.getElementById('skills')
+let displaySkills = document.querySelector('.mes_skills')
+
+console.log(displayStack)
+console.log(displayOutils)
+console.log(displaySkills)
+
+buttonMaStack.addEventListener ('click', ()=>{
+    displayStack.classList.remove('hide')
+    displayOutils.classList.add('hide')
+    displaySkills.classList.add('hide')
+}
+)
+
+buttonOutils.addEventListener ('click', ()=>{
+    displayStack.classList.add('hide')
+    displayOutils.classList.remove('hide')
+    displaySkills.classList.add('hide')
+}
+)
+
+buttonSkills.addEventListener ('click', ()=>{
+    displayStack.classList.add('hide')
+    displayOutils.classList.add('hide')
+    displaySkills.classList.remove('hide')
+}
+)
 
 
-    // else {
-   
-                
-    //     },400)
-    //     body.classList.remove('yellowTheme')
-    //     body.classList.add('blueTheme')
-    //     header.classList.remove('headery')
-    //     header.classList.add('headerb')
-        
-       
 
-    //     blue = true
-        
-    // }
-    // console.log(blue)
 
+// troisième section : au clique sur l'image de la stack afficher le texte qui lui correspond 
+
+const ImgToText = {
+    'img_js' : 'txt_js',
+    'img_python' : 'txt_python'
+}
+let stack_txt = document.querySelectorAll(".stack_txt span")
+let stack_img = document.querySelectorAll(".stack_img img")
+
+
+for (let index = 0; index < stack_img.length; index++) { //On parcours l'enssemble des image clicable
+    const interactible_img = stack_img[index];
+    interactible_img.addEventListener ('click',function() { //On attache au image clicable un event listener 
+        for (let i = 0; i < stack_txt.length; i++) { // Boucle pour cacher tout les éléments texte lié aux images 
+            const txtForImg = stack_txt[i];
+            txtForImg.classList.add('hide')
+        }
+        let imgID= this.getAttribute('id') //récupération de l'ID lié à l'image cliqué 
+        let txt = ImgToText[imgID] // récupéré l'id du txt correspondant à l'img 
+
+        let TargetTxt = document.getElementById(txt) // on selectionne l'élément grace à l'id récupérer dans l'objet ImgToText
+        console.log(imgID)
+        TargetTxt.classList.remove('hide') // aficher le texte
+
+    })}
 
 
